26f3c580011574f82fb8d6c1e4a33b1976ae25be
Falaki <farshad.falaki@gmail.com>
2020-11-23 16:04:46 +0100

    first commit

---
+++ src/main/java/com/farshad/simpeweb/SimpeWebApplication.java
@@ -0,0 +1,21 @@
+package com.farshad.simpeweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SimpeWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpeWebApplication.class, args);
+    }
+
+    @GetMapping(value = "/simpe/hi")
+    public String sayHi(){
+        return "Hallo!";
+    }
+
+}
---
+++ src/test/java/com/farshad/simpeweb/SimpeWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farshad.simpeweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpeWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
